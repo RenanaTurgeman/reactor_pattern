d11cf9f9b994e29f37df30ad1442d7b31f131fac
TamarBarIlan <tamarbarilan.4@gmail.com>
2023-05-18 14:06:07 +0300

    server and reactor works!

--- server.c
+++ server.c
@@ -119,137 +119,6 @@ void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
     (*fd_count)--;
 }
 
-/*
-int handler_server(void) // handler_server
-{
-    int listener; // Listening socket descriptor
-
-    int newfd;                          // Newly accept()ed socket descriptor
-    struct sockaddr_storage remoteaddr; // Client address
-    socklen_t addrlen;
-
-    char buf[256]; // Buffer for client data
-
-    char remoteIP[INET6_ADDRSTRLEN];
-
-    // Start off with room for 5 connections
-    // (We'll realloc as necessary)
-    int fd_count = 0;
-    int fd_size = 5;
-    struct pollfd *pfds = malloc(sizeof *pfds * fd_size);
-
-    // Set up and get a listening socket
-    listener = get_listener_socket();
-
-    if (listener == -1)
-    {
-        fprintf(stderr, "error getting listening socket\n");
-        exit(1);
-    }
-
-    // Add the listener to set
-    pfds[0].fd = listener;
-    pfds[0].events = POLLIN; // Report ready to read on incoming connection
-
-    fd_count = 1; // For the listener
-
-    // Main loop
-    for (;;)
-    {
-        int poll_count = poll(pfds, fd_count, -1);
-
-        if (poll_count == -1)
-        {
-            perror("poll");
-            exit(1);
-        }
-
-        // Run through the existing connections looking for data to read
-        for (int i = 0; i < fd_count; i++)
-        {
-
-            // Check if someone's ready to read
-            if (pfds[i].revents & POLLIN)
-            { // We got one!!
-
-                if (pfds[i].fd == listener)
-                {
-                    // If listener is ready to read, handle new connection
-
-                    addrlen = sizeof remoteaddr;
-                    newfd = accept(listener,
-                                   (struct sockaddr *)&remoteaddr,
-                                   &addrlen);
-
-                    if (newfd == -1)
-                    {
-                        perror("accept");
-                    }
-                    else
-                    {
-                        add_to_pfds(&pfds, newfd, &fd_count, &fd_size);
-
-                        printf("pollserver: new connection from %s on "
-                               "socket %d\n",
-                               inet_ntop(remoteaddr.ss_family,
-                                         get_in_addr((struct sockaddr *)&remoteaddr),
-                                         remoteIP, INET6_ADDRSTRLEN),
-                               newfd);
-                    }
-                }
-                else
-                {
-                    // If not the listener, we're just a regular client
-                    int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
-
-                    int sender_fd = pfds[i].fd;
-
-                    if (nbytes <= 0)
-                    {
-                        // Got error or connection closed by client
-                        if (nbytes == 0)
-                        {
-                            // Connection closed
-                            printf("pollserver: socket %d hung up\n", sender_fd);
-                        }
-                        else
-                        {
-                            perror("recv");
-                        }
-
-                        close(pfds[i].fd); // Bye!
-
-                        del_from_pfds(pfds, i, &fd_count);
-                    }
-                    else
-                    {
-                        // We got some good data from a client
-                        buf[nbytes] = '\0'; // Null-terminate the data
-                        printf("Received data: %s\n", buf);
-
-                        for (int j = 0; j < fd_count; j++)
-                        {
-                            // Send to everyone!
-                            int dest_fd = pfds[j].fd;
-
-                            // Except the listener and ourselves
-                            if (dest_fd != listener && dest_fd != sender_fd)
-                            {
-                                // If you want to print the data to screen instead of sending it to all clients
-                                // We moved this block of code up, so we are not using it here anymore.
-                            }
-                        }
-                    }
-                }
-                // END handle data from client
-            } // END got ready-to-read from poll()
-        }     // END looping through file descriptors
-    }         // END for(;;)--and you thought it would never end!
-
-    return 0;
-}
-*/
-
 int handler_server(int listener) // handler_server
 {
     int newfd;                          // Newly accept()ed socket descriptor
@@ -274,51 +143,6 @@ int handler_server(int listener) // handler_server
         addFd(globalReactor, newfd, &handler_client);
     }
 }
-
-// int handler_clinent()
-// {
-// // If not the listener, we're just a regular client
-// int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
-
-// int sender_fd = pfds[i].fd;
-
-// if (nbytes <= 0)
-// {
-//     // Got error or connection closed by client
-//     if (nbytes == 0)
-//     {
-//         // Connection closed
-//         printf("pollserver: socket %d hung up\n", sender_fd);
-//     }
-//     else
-//     {
-//         perror("recv");
-//     }
-
-//     close(pfds[i].fd); // Bye!
-
-//     del_from_pfds(pfds, i, &fd_count);
-// }
-// else
-// {
-//     // We got some good data from a client
-//     buf[nbytes] = '\0'; // Null-terminate the data
-//     printf("Received data: %s\n", buf);
-
-//     for (int j = 0; j < fd_count; j++)
-//     {
-//         // Send to everyone!
-//         int dest_fd = pfds[j].fd;
-
-//         // Except the listener and ourselves
-//         if (dest_fd != listener && dest_fd != sender_fd)
-//         {
-//             // If you want to print the data to screen instead of sending it to all clients
-//             // We moved this block of code up, so we are not using it here anymore.
-//         }
-//     }
-// }
-// }
 
 int handler_client(int fd)
 {
@@ -363,8 +187,8 @@ int main()
         exit(1);
     }
 
-    startReactor(globalReactor);
     addFd(globalReactor, listener, handler_server);
+    startReactor(globalReactor);
     waitFor(globalReactor);
     printf("Type something and press Enter. It will be echoed back to you.\n");
     freeReactor(globalReactor);
